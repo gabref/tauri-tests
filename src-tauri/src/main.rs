80db12117f12547dd5f6238052b7a59608916be1
gabref <gabrielfranzer@gmail.com>
2024-05-05 22:31:39 +0200

    no blur on windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -24,9 +24,9 @@ fn main() {
             apply_vibrancy(&_window, NSVisualEffectMaterial::HudWindow, None, None)
                 .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
-            #[cfg(target_os = "windows")]
-            apply_blur(&_window, Some((18, 18, 18, 125)))
-                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+            // #[cfg(target_os = "windows")]
+            // apply_blur(&_window, Some((18, 18, 18, 125)))
+            //     .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
             Ok(())
         })
