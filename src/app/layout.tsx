5968c6484832ad7725e688657174ad45474f7960
gabref <gabrielfranzer@gmail.com>
2024-05-05 23:05:29 +0200

    add reference to solution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import dynamic from 'next/dynamic'
 
 const inter = Inter({ subsets: ["latin"] });
+// https://medium.com/@emdadulislam162/resolving-window-is-not-defined-error-during-npm-run-build-in-next-js-ssr-application-67e2c6197425
 const TitleBar = dynamic(() => import('@/components/titlebar'), { 
 	ssr: false 
 });
