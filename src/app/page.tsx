6f0b47139614243c5917d58149d6f939e4c780b7
gabref <gabrielfranzer@gmail.com>
2024-05-13 12:50:01 +0200

    finally, correct router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { listen } from '@tauri-apps/api/event'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Home() {
